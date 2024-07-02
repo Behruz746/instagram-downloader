8a45d4b2cd87eea560ef1e56e59eb880b9e320ab
Behruz <madaminovbehruz746@gmail.com>
2024-07-02 11:54:48 +0500

    language files

---
+++ src/constans/index.jsx
@@ -0,0 +1,16 @@
+const images = {
+  Logo: "/img/webp/logo.webp",
+  lightLogo: "/img/webp/light-logo.webp",
+  imgAbout1: "/img/webp/about-img01.webp",
+  imgAbout2: "/img/webp/about-img02.webp",
+};
+
+const icons = {
+  titleLogo: "/img/svg/title-logo.svg",
+  webLogo: "/img/svg/web-logo.svg",
+  arrow: "/img/svg/arrow-icon.svg",
+  webSiteLogo: "/img/svg/behruz-logo.svg",
+  facebook: "/img/svg/facebook-logo.svg",
+  instagram: "/img/svg/instagram-logo.svg",
+  telegram: "/img/svg/telegram-logo.svg",
+};
--- src/main.jsx
+++ src/main.jsx
@@ -8,21 +8,21 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
 
-// i18next
-//   .use(initReactI18next)
-//   .use(LanguageDetector)
-//   .use(HttpApi)
-//   .init({
-//     supportedLngs: ["en", "uz", "ru"],
-//     fallbackLng: "en",
-//     detection: {
-//       order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
-//       caches: ["localStorage"],
-//     },
-//     backend: { loadPath: "/locales/{{lng}}/translation.json" },
-//     react: { useSuspense: false },
-//   });
-// document.documentElement.lang = localStorage.getItem("i18nextLng");
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "uz", "ru", "jp", "ar", "chi", "kr", "tr", "uk"],
+    fallbackLng: "uz",
+    detection: {
+      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
+      caches: ["localStorage"],
+    },
+    backend: { loadPath: "/locales/{{lng}}/translation.json" },
+    react: { useSuspense: false },
+  });
+document.documentElement.lang = localStorage.getItem("i18nextLng");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
